10e5462f76ead6bdd506e958666030bf093ce639
Sagar-Gyanchandani <59876433+Sagar-Gyanchandani@users.noreply.github.com>
2021-03-24 01:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def predict():
             result = ValuePredictor(to_predict_list,7)
 
     if(int(result)==1):
-        # prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
+        prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
         return(render_template("result_bad.html", prediction_text=prediction))
     else:
-#         prediction = "No need to fear. You have no dangerous symptoms of the disease"
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
         return(render_template("result_good.html", prediction_text=prediction))
 
 if __name__ == "__main__":
